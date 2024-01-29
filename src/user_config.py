ee7981f7dc3375d466003a17a7fdf10382cc498c
Johnny Howe <jonathon.o.howe@gmail.com>
2024-01-29 17:35:25 +1300

    Adjusted config

--- src/user_config.py
+++ src/user_config.py
@@ -1,8 +1,8 @@
 class _UserConfig:
     _data = {
         "juice_screen_movement_from_paddle_lerp_speed": 8,
-        "juice_screen_movement_from_paddle_max_vertical": 0.5,
-        "juice_screen_movement_from_paddle_max_rotation_degrees": 6
+        "juice_screen_movement_from_paddle_max_vertical": 0.3,
+        "juice_screen_movement_from_paddle_max_rotation_degrees": 2
     }
 
     def __init__(self):
