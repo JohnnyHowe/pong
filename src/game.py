971ee384a18f640a51e0228d3149efd8276e9ab1
Johnny Howe <jonathon.o.howe@gmail.com>
2024-01-30 10:58:30 +1300

    screen nudge on ball collision

--- src/game.py
+++ src/game.py
@@ -18,6 +18,8 @@ class Game:
         self.player1 = Player(KeyboardPlayerInput(pygame.K_w, pygame.K_s), position=(-6, 0))
         self.player2 = Player(KeyboardPlayerInput(pygame.K_UP, pygame.K_DOWN), position=(6, 0))
         self.reset_ball()
+        self.board_ball_knock = 0
+        self.board_ball_knock_rotation = 0
 
     def reset_ball(self):
         self.ball = Ball()
@@ -30,28 +32,37 @@ class Game:
     def step(self):
         self.run_event_loop()
 
+        self.player1.update()
+        self.player2.update()
+        self.ball.update([self.player1, self.player2])
+
+        # visuals
+        window._draw_buffer.fill((0, 0, 0))
+        window.fill_undefined_area((150, 150, 150))
+
+        self.player1.draw()
+        self.player2.draw()
+        self.ball.show()
+
+        # juice
         rotation_effect = math.radians(JUICE_SCREEN_MOVEMENT_FROM_PADDLE_MAX_ROTATION_DEGREES / 2)
         p1_effect = self.get_player_rotation_effect(self.player1)
         p2_effect = self.get_player_rotation_effect(self.player2)
         target_camera_rotation_rads = (p1_effect - p2_effect) * rotation_effect
         window.camera_rotation_rads = self.lerp(window.camera_rotation_rads, target_camera_rotation_rads, clock.dt_seconds * JUICE_SCREEN_MOVEMENT_FROM_PADDLE_LERP_SPEED)
 
-        movement = p1_effect + p2_effect 
+        screen_movement_effect = p1_effect + p2_effect 
         target_camera_vertical_position = 0
-        if abs(movement) == 2: target_camera_vertical_position = (movement / 2) * JUICE_SCREEN_MOVEMENT_FROM_PADDLE_MAX_VERTICAL
+        if abs(screen_movement_effect) == 2: target_camera_vertical_position = (screen_movement_effect / 2) * JUICE_SCREEN_MOVEMENT_FROM_PADDLE_MAX_VERTICAL
+
+        self.board_ball_knock = self.lerp(self.board_ball_knock, 0, clock.dt_seconds * JUICE_SCREEN_MOVEMENT_FROM_BALL_KNOCK_RETURN_LERP_SPEED)
+        if self.ball.position[1] + self.ball.size / 2 >= window.game_size[1] / 2:
+            self.board_ball_knock = -JUICE_SCREEN_MOVEMENT_FROM_BALL_KNOCK
+        if self.ball.position[1] - self.ball.size / 2 <= -window.game_size[1] / 2:
+            self.board_ball_knock = JUICE_SCREEN_MOVEMENT_FROM_BALL_KNOCK
+        target_camera_vertical_position += self.board_ball_knock
+
         window.camera_position = (0, self.lerp(window.camera_position[1], target_camera_vertical_position, clock.dt_seconds * JUICE_SCREEN_MOVEMENT_FROM_PADDLE_LERP_SPEED))
-
-        window._draw_buffer.fill((0, 0, 0))
-        window.fill_undefined_area((150, 150, 150))
-
-        self.player1.update()
-        self.player2.update()
-        self.ball.update([self.player1, self.player2])
-
-        self.player1.draw()
-        self.player2.draw()
-        self.ball.show()
-
         window.update()
 
         if (self.ball.is_off_screen_horizontal()):
@@ -70,7 +81,6 @@ class Game:
                 quit()
             if event.type == pygame.VIDEORESIZE:
                 window.set_size((event.w, event.h))
-
 
     def lerp(self, a, b, t):
         return a + (b - a) * t
