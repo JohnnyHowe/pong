b46eff0f7f940817863d842d01e540c65271a431
Johnny Howe <jonathon.o.howe@gmail.com>
2024-01-31 10:29:40 +1300

    Build script comments

--- compile.bat
+++ compile.bat
@@ -1,16 +1,19 @@
 @echo off
 
+@REM This section here creates a little wee virtual environment. It's not required, but makes the builds more consistent
 set PYTHON=python
 set VENV_NAME=venv
 %PYTHON% -m venv %VENV_NAME%
 call %VENV_NAME%\Scripts\activate
 
+@REM Install required packages (can't use the ones installed on your computer because it's in a virtual environment)
 echo "Installing dependencies..."
-pip install pygame
 pip install nuitka
+pip install pygame
 
 echo "Compiling ..."
 nuitka --standalone --include-data-dir=./assets=assets main.py
 
+@REM Deactivating the virtual environment
 echo "Done, deactivating..."
 deactivate
