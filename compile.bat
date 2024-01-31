5bf238e5af9bcc4d224a1789733efb19e7c0a000
Johnny Howe <jonathon.o.howe@gmail.com>
2024-01-31 18:13:18 +1300

    Compile script also compresses

--- compile.bat
+++ compile.bat
@@ -1,12 +1,10 @@
 @echo off
 
-@REM This section here creates a little wee virtual environment. It's not required, but makes the builds more consistent
 set PYTHON=python
 set VENV_NAME=venv
 %PYTHON% -m venv %VENV_NAME%
 call %VENV_NAME%\Scripts\activate
 
-@REM Install required packages (can't use the ones installed on your computer because it's in a virtual environment)
 echo "Installing dependencies..."
 pip install nuitka
 pip install pygame
@@ -14,6 +12,8 @@ pip install pygame
 echo "Compiling ..."
 nuitka --standalone --include-data-dir=./assets=assets main.py
 
-@REM Deactivating the virtual environment
+echo "Tidying up ..."
+powershell Compress-Archive -Path "main.dist" -DestinationPath "main.dist.zip"
+
 echo "Done, deactivating..."
 deactivate
